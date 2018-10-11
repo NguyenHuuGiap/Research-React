120ac2dd75b4be0a7ae6fa3f132315c056e82ea6
Nguyen Huu Giap <nguyenhuugiap4@gmail.com>
2018-10-11 14:26:40 +0700

    Session 2 - App request with rails

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import VideoListItem from './video_list_item';
 
-const Videos = (props) => {
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return (
+      < VideoListItem
+      onVideoSelect={props.onVideoSelect}
+      key={video.etag}
+      video={video} />
+    )
+  })
+
   return (
-    <ul>
-      {props.videos.length}
+    <ul className="col-md-4 list-group">
+      {videoItems}
     </ul>
   )
 }
+
+export default VideoList
